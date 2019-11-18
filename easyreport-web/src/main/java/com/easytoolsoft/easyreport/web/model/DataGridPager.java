6c69560e67ba118ef072ef5b419b882a08284b71
koqiui <koqiui@163.com>
2019-11-18 20:46:42 +0800

    修正后端DataGridPager参数（分页信息）接收无效问题

--- easyreport-web/src/main/java/com/easytoolsoft/easyreport/web/model/DataGridPager.java
+++ easyreport-web/src/main/java/com/easytoolsoft/easyreport/web/model/DataGridPager.java
@@ -1,12 +1,16 @@
 package com.easytoolsoft.easyreport.web.model;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.easytoolsoft.easyreport.mybatis.pager.PageInfo;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * easyui gridview控件分页类 <br>
+ * 
+ * 字段改为NONE final（否则从前台拿不到分页信息）
  *
  * @author Tom Deng
  * @date 2017-03-25
@@ -14,20 +18,19 @@ import org.apache.commons.lang3.StringUtils;
 @Data
 @NoArgsConstructor
 public class DataGridPager {
-    private final Integer page = 1;
-    private final Integer rows = 50;
-    private final String sort = "id";
-    private final String order = "desc";
+	private Integer page = 1;
+	private Integer rows = 50;
+	private String sort = "id";
+	private String order = "desc";
 
-    public PageInfo toPageInfo() {
-        return this.toPageInfo("");
-    }
+	public PageInfo toPageInfo() {
+		return this.toPageInfo("");
+	}
 
-    public PageInfo toPageInfo(final String tablePrefix) {
-        final String prefix = StringUtils.defaultString(tablePrefix, "").trim();
-        final String name = StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(this.sort), '_');
-        final String sortField = prefix + StringUtils.defaultString(name, "").toLowerCase();
-        return new PageInfo((this.page - 1) * this.rows,
-            this.rows, sortField, this.order);
-    }
+	public PageInfo toPageInfo(final String tablePrefix) {
+		final String prefix = StringUtils.defaultString(tablePrefix, "").trim();
+		final String name = StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(this.sort), '_');
+		final String sortField = prefix + StringUtils.defaultString(name, "").toLowerCase();
+		return new PageInfo((this.page - 1) * this.rows, this.rows, sortField, this.order);
+	}
 }
